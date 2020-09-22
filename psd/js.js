d9f0aba2011004de8bd5819c47f55761dc54b5a7
Dimas7704 <68776836+Dimas7704@users.noreply.github.com>
2020-09-22 18:55:01 +0300

    Add files via upload

---
+++ psd/js.js
@@ -0,0 +1,21 @@
+/*var array = new Array("css/screen_first.png", "css/screen_second.png", "css/screen_three.png", "css/screen_four.png");
+delay = 3500;
+
+function imgRotator(i){
+	var img = document.getElementById("image_rotator");
+	img.src = array[i];
+	i++;
+	if (i == array.length) i=0;
+	setTimeout("imgRotator("+i+")", delay);
+}
+setTimeout('a()', 2000);
+function a(){
+	window.location.reload();
+}
+
+var btn_upload = document.getElementById("upload");
+btn_upload.addEventListener("click", upload_img);
+	
+function upload_img(){
+	 var keyBox = document.search.key;
+}*/
